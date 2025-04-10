ea2f17157d0274b81cfafb3c394d38b3680dfce5
LuizHNR <luizhneri12@gmail.com>
2025-04-10 11:41:06 -0300

    Documentação

--- fiap.c2/fiap.c2/src/main/java/br/com/fiap/c2/Luizinho123Application.java
+++ fiap.c2/fiap.c2/src/main/java/br/com/fiap/c2/Luizinho123Application.java
@@ -1,11 +1,15 @@
 package br.com.fiap.c2;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "br.com.fiap.c2.model")
+@OpenAPIDefinition(info =
+@Info(title = "API de Clientes", description = "Exemplo de API RESTful com Swagger", version = "v1"))
 public class Luizinho123Application {
 
 	public static void main(String[] args) {
